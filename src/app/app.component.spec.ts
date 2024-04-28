fb07fc63308cb890f02808861e256e59b21881c7
msl-ltd <msl-ltd@outlook.jp>
2024-04-28 12:52:27 +0900

    chore(angular): npx -p @angular/cli@16 ng new作成差分をv16に合わせる

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,14 +3,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -28,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular app is running!'
+    );
   });
 });
